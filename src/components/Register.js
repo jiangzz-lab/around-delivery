ce860bc1bf44ff53324d5211d23a79e6e442b713
vivianz <vivianxinzhang@gmail.com>
2020-07-01 11:52:04 -0700

    add register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,13 +1,74 @@
 import React, {Component} from 'react';
+import { Form, Input, Button } from 'antd';
 
-class Register extends Component {
+class RegistrationForm extends Component {
     render() {
+        const { getFieldDecorator } = this.props.form;
+
+        const formItemLayout = {
+            labelCol: {
+                xs: { span: 24 },
+                sm: { span: 8 },
+            },
+            wrapperCol: {
+                xs: { span: 24 },
+                sm: { span: 16 },
+            },
+        };
+
+        const tailFormItemLayout = {
+            wrapperCol: {
+                xs: {
+                    span: 24,
+                    offset: 0,
+                },
+                sm: {
+                    span: 16,
+                    offset: 8,
+                },
+            },
+        };
+
         return (
-            <div>
-                Register changed on branch
-            </div>
+            <Form {...formItemLayout} onSubmit={this.handleSubmit} className="register">
+                <Form.Item label="Username">
+                    {
+                        getFieldDecorator('Username', {})(<Input />)
+                    }
+                </Form.Item>
+                <Form.Item label="Password" hasFeedback>
+                    {
+                        getFieldDecorator('Password', {
+                            rule: [
+                                {
+                                    validator: this.validateToNextPassword,
+                                }
+                            ]
+                        })(<Input />)
+                    }
+                </Form.Item>
+                <Form.Item label="Confirm Password" hasFeedback>
+                    {
+                        getFieldDecorator('Confirm Password', {
+                            rules: [
+                                {
+                                    required: true,
+                                    message: 'Please verify your Password'
+                                }
+                            ]
+                        })(<Input />)
+                    }
+                </Form.Item>
+                <Form.Item {...tailFormItemLayout}>
+                    <Button type="primary" htmlType="submit">
+                        Register
+                    </Button>
+                </Form.Item>
+            </Form>
         );
     }
 }
+
+const Register = Form.create({ name: 'register' })(RegistrationForm);
 
 export default Register;
