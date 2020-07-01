b1b0e2c3de16fa5ee6cc12f88c5f2efe1ab85b47
Zhanzhi <hnzjzz@gmail.com>
2020-07-01 12:11:19 -0500

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	new file:   src/components/CheckOut.js 	new file:   src/components/Confirm.js 	modified:   src/components/Order.js 	new file:   src/components/Payment.js 	new file:   src/components/Recommend.js 	new file:   src/components/ShipInfo.js 	new file:   src/styles/CheckOut.css 	new file:   src/styles/Confirm.css 	new file:   src/styles/ShipInfo.css 	new file:   src/styles/recommend.css

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,5 +1,11 @@
 import React, {Component} from 'react'
 
+import ShipInfo from "./ShipInfo";
+import Recommend from "./Recommend";
+import CheckOut from "./CheckOut";
+import Payment from "./Payment";
+import Confirm from "./Confirm";
+
 import { Steps, Button, message } from 'antd';
 
 const { Step } = Steps;
@@ -46,7 +52,11 @@ class Order extends Component {
     }
 
     render() {
+        {/* current notes the current step of the order process*/}
         const { current } = this.state;
+        {/* stepContent is an Array that saves corresponding component to render
+         as step content for each step*/}
+        const stepContent = [<ShipInfo />, <Recommend />, <CheckOut />, <Payment />, <Confirm />];
         return (
             <div>
                 <div className="order-steps">
@@ -55,9 +65,12 @@ class Order extends Component {
                             <Step key={item.title} title={item.title} />))}
                     </Steps>
                 </div>
+                {/* This is the place to render content for the current step;
+                    it could be ShipInfo, Recommend etc according to the value of current*/}
                 <div className="steps-content">
-                    {steps[current].content}
+                    {stepContent[current]}
                 </div>
+
                 <div className="steps-action">
                     {current < steps.length - 1 && (
                         <Button type="primary" onClick={() => this.next()}>
---
+++ src/components/ShipInfo.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ShipInfo extends Component {
+    render() {
+        return (
+            <div>
+                Ship Info
+            </div>
+        );
+    }
+}
+
+export default ShipInfo;
