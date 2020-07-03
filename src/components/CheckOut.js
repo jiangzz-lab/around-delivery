b8d1ae756e2e4a36f3c3a912dc161f5d5f4a817b
vivianz <vivianxinzhang@gmail.com>
2020-07-03 13:56:43 -0700

    add checkout

--- src/components/CheckOut.js
+++ src/components/CheckOut.js
@@ -4,15 +4,29 @@ import { Descriptions } from 'antd';
 class CheckOut extends Component {
     render() {
         return (
-            <Descriptions title="User Info">
-                <Descriptions.Item label="UserName">Zhou Maomao</Descriptions.Item>
-                <Descriptions.Item label="Telephone">1810000000</Descriptions.Item>
-                <Descriptions.Item label="Live">Hangzhou, Zhejiang</Descriptions.Item>
-                <Descriptions.Item label="Remark">empty</Descriptions.Item>
-                <Descriptions.Item label="Address">
-                    No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
-                </Descriptions.Item>
-            </Descriptions>
+            <div>
+                <Descriptions title="Receiver Info">
+                    <Descriptions.Item label="Name">haha</Descriptions.Item>
+                    <Descriptions.Item label="Telephone">1810000000</Descriptions.Item>
+                    <Descriptions.Item label="Remark"></Descriptions.Item>
+                    <Descriptions.Item label="Address">
+                        No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
+                    </Descriptions.Item>
+                </Descriptions>
+                <Descriptions title="Sender Info">
+                    <Descriptions.Item label="Name">Your friend</Descriptions.Item>
+                    <Descriptions.Item label="Telephone">1810000000</Descriptions.Item>
+                    <Descriptions.Item label="Email">xxxx@gmail.com</Descriptions.Item>
+                    <Descriptions.Item label="Address">
+                        1600 Amphitheatre Pkwy, Mountain View, CA 94043
+                    </Descriptions.Item>
+                </Descriptions>
+                <Descriptions title="Package Info">
+                    <Descriptions.Item label="Delivery Time">1h</Descriptions.Item>
+                    <Descriptions.Item label="Delivery Fee">$18</Descriptions.Item>
+                    <Descriptions.Item label="Carrier"> drone </Descriptions.Item>
+                </Descriptions>
+            </div>
         );
     }
 }
