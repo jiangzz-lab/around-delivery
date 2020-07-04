3366a91be6d2ff575972ca6b1b4156665e16f217
vivianz <vivianxinzhang@gmail.com>
2020-07-03 20:13:18 -0700

    update

--- src/components/CheckOut.js
+++ src/components/CheckOut.js
@@ -3,19 +3,20 @@ import { Descriptions } from 'antd';
 
 class CheckOut extends Component {
     render() {
+        
         return (
             <div>
                 <Descriptions title="Receiver Info">
-                    <Descriptions.Item label="Name">Apple</Descriptions.Item>
+                    <Descriptions.Item label="Name">Facebook</Descriptions.Item>
                     <Descriptions.Item label="Telephone">xxxxxxxxxx</Descriptions.Item>
-                    <Descriptions.Item label="Remark"></Descriptions.Item>
+                    <Descriptions.Item label="Email">xxxx@gmail.com</Descriptions.Item>
                     <Descriptions.Item label="Address">
-                        One Apple Park Way, Cupertino, CA 95014
+                        1 Hacker Way, Menlo Park, CA 94025
                     </Descriptions.Item>
                 </Descriptions>
                 <Descriptions title="Sender Info">
                     <Descriptions.Item label="Name">Google</Descriptions.Item>
-                    <Descriptions.Item label="Telephone">xxxxxxxx</Descriptions.Item>
+                    <Descriptions.Item label="Telephone">xxxxxxxxxx</Descriptions.Item>
                     <Descriptions.Item label="Email">xxxx@gmail.com</Descriptions.Item>
                     <Descriptions.Item label="Address">
                         1600 Amphitheatre Pkwy, Mountain View, CA 94043
@@ -27,8 +28,6 @@ class CheckOut extends Component {
                     <Descriptions.Item label="Carrier"> drone </Descriptions.Item>
                 </Descriptions>
             </div>
-
-            
         );
     }
 }
